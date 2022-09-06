ef3ab66c4a9f9fbdd6bbc6952a911529254f9a75
Gabriel Garcia <gabrielgarciaofc@gmail.com>
2022-09-06 20:21:46 -0300

    Database seeding

--- backend/src/main/java/com/projetogarcia/dscatalog/service/CategoryService.java
+++ backend/src/main/java/com/projetogarcia/dscatalog/service/CategoryService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.projetogarcia.dscatalog.entities.Category;
 import com.projetogarcia.dscatalog.repositories.CategoryRepository;
@@ -16,8 +17,8 @@ public class CategoryService {
 	@Autowired // Injetar dependencia valida do CategoryRepository
 	private CategoryRepository repository;
 	
-	// método para acessar o repository e chamar no banco de dados as categorias
-	public List<Category> findAll(){ 
+	@Transactional(readOnly = true) // Dizer para o sistema que esse método está ligado com o banco de dados / ReadOnly é para não travar o bdd somente para ler 
+	public List<Category> findAll(){ // método para acessar o repository e chamar no banco de dados as categorias
 		return repository.findAll(); 
 	} // funcionalidade vinda do JpaRepository na Category
 	

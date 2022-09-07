6e34a9b886bf9530479ad32b8b3fb25c266b5719
Gabriel Garcia <gabrielgarciaofc@gmail.com>
2022-09-07 00:26:21 -0300

    DTO

--- backend/src/main/java/com/projetogarcia/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/projetogarcia/dscatalog/resources/CategoryResource.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.projetogarcia.dscatalog.entities.Category;
+import com.projetogarcia.dscatalog.DTO.CategoryDTO;
 import com.projetogarcia.dscatalog.service.CategoryService;
 
 @RestController // Avisar que vai ser uma class Rest
@@ -19,8 +19,8 @@ public class CategoryResource {
 	private CategoryService service;
 	
 	@GetMapping
-	public ResponseEntity<List<Category>> findAll(){  // Método http / findAll()Busca todas categorias 
-		List<Category> list = service.findAll();
+	public ResponseEntity<List<CategoryDTO>> findAll(){  // Método http / findAll()Busca todas categorias 
+		List<CategoryDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list); // OK() é uma resposta 200
 	}
 }
--- backend/src/main/java/com/projetogarcia/dscatalog/service/CategoryService.java
+++ backend/src/main/java/com/projetogarcia/dscatalog/service/CategoryService.java
@@ -1,14 +1,15 @@
 package com.projetogarcia.dscatalog.service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.projetogarcia.dscatalog.DTO.CategoryDTO;
 import com.projetogarcia.dscatalog.entities.Category;
 import com.projetogarcia.dscatalog.repositories.CategoryRepository;
-
 
 
 @Service // Vai registrar como um componente que vai participar do sistema de injeção de dependência automatizado do spring
@@ -17,9 +18,19 @@ public class CategoryService {
 	@Autowired // Injetar dependencia valida do CategoryRepository
 	private CategoryRepository repository;
 	
+	
 	@Transactional(readOnly = true) // Dizer para o sistema que esse método está ligado com o banco de dados / ReadOnly é para não travar o bdd somente para ler 
-	public List<Category> findAll(){ // método para acessar o repository e chamar no banco de dados as categorias
-		return repository.findAll(); 
+	public List<CategoryDTO> findAll(){ // método para acessar o repository e chamar no banco de dados as categorias
+	
+		List<Category> list = repository.findAll(); 
+		
+		// Transformar o List Category em CategoryDTO
+		
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());  // stream para funções de alta ordem -> map para transformar cada elemento original em outra coisa (aplica uma função a cada elemento da lista)
+																			// Collect para transformar o stream em lista novamente					
 	} // funcionalidade vinda do JpaRepository na Category
 	
 }
+
+
+//List<Entidade>
